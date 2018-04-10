7049307a491e6cb2ec367f32f18e4d9dab710e1d
Tyler Westlie <tyler.westlie@gmail.com>
2018-04-10 15:43:07 -0600

    working on InvoiceItem

--- lib/invoice_item.rb
+++ lib/invoice_item.rb
@@ -1,8 +1,14 @@
+require 'time'
 
 class InvoiceItem
   attr_reader :id,
               :item,
-              :invoice_id
+              :invoice_id,
+              :quantity,
+              :unit_price,
+              :created_at,
+              :updated_at
+
   def initialize(data)
     @id = data[:id].to_i
     @item = data[:item]
@@ -11,6 +17,10 @@ class InvoiceItem
     @unit_price = data[:unit_price]
     @created_at = Time.parse(data[:created_at])
     @updated_at = Time.parse(data[:updated_at])
+  end
+
+  def unit_price_to_dollars
+    @unit_price.to_f
   end
 
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -3,16 +3,21 @@ require 'minitest/pride'
 require './lib/invoice_item'
 require 'time'
 
-class ItemInvoiceTest < Minitest::Test
+class InvoiceItemTest < Minitest::Test
+
   def test_exists
-    ii = ItemInvoice.new({:id => 1,
+    ii = InvoiceItem.new({:id => 1,
                           :item_id => 263519844,
                           :invoice_id => 1,
                           :quantity => 5,
-                          :unit_price => 13635,
+                          :unit_price => 13635
                           :created_at => Time.now,
                           :updated_at => Time.now
                           })
-    assert_instance_of ItemInvoice, ii
+
+      assert_instance_of InvoiceItem, ii
+    # assert_instance_of Time, ii.created_at
+    # assert_instance_of Time, ii.updated_at
   end
+
 end
